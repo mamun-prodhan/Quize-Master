9c0181b2b01002562cf884d625afc1ac4104ca42
mamun-prodhan <mamunpr9@gmail.com>
2022-10-19 19:57:32 +0600

    favicon added & navbar bug fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,12 @@ const Header = () => {
     return (
         <nav className='p-3  bg-success text-dark bg-opacity-75 d-flex justify-content-between align-items-center'>
             <div>
-                <h3 className='fw-bold'>Skill Tester</h3>
+                <h3 className='fw-bold'>Quiz Master</h3>
             </div>
             <div>
-                <Link to='/' className='text-decoration-none me-3 text-dark fw-semibold'>Topics</Link>
-                <Link to='/statistics' className='text-decoration-none me-3 text-dark fw-semibold'>Statistics</Link>
-                <Link to='/blog'  className='text-decoration-none me-3 text-dark fw-semibold'>Blog</Link>
+                <Link to='/' className='text-decoration-none me-3 fw-bold btn btn-outline-primary'>Topics</Link>
+                <Link to='/statistics' className='text-decoration-none me-3 fw-bold btn btn-outline-primary'>Statistics</Link>
+                <Link to='/blog'  className='text-decoration-none me-3 fw-bold btn btn-outline-primary'>Blog</Link>
             </div>
         </nav>
     );
