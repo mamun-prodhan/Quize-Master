ee141733dfb08887b1b6bb25cb4664d706ec4a5c
mamun-prodhan <mamunpr9@gmail.com>
2022-10-19 17:53:29 +0600

    answer toast completed

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
 import Options from '../Options/Options';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faEye } from '@fortawesome/free-solid-svg-icons'
 
 // toast import
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const Quiz = ({data, index}) => {
+const Quiz = ({ data, index }) => {
     // console.log(data);
     // console.log(index);
     const options = data.options;
@@ -17,9 +18,9 @@ const Quiz = ({data, index}) => {
 
     // console.log(quizvalue);
 
-    const quizHandler = (event) =>{
+    const quizHandler = (event) => {
         console.log(event);
-        if(event === correctAnswer){
+        if (event === correctAnswer) {
             toast.success('WoW !!! Right Answer', {
                 position: "top-center",
                 autoClose: 2000,
@@ -29,9 +30,9 @@ const Quiz = ({data, index}) => {
                 draggable: true,
                 progress: undefined,
                 theme: "light",
-                });
+            });
         }
-        else{
+        else {
             toast.error('Wrong Answer !!!!', {
                 position: "top-center",
                 autoClose: 2000,
@@ -41,8 +42,21 @@ const Quiz = ({data, index}) => {
                 draggable: true,
                 progress: undefined,
                 theme: "light",
-                });
+            });
         }
+    }
+
+    const eyeHandler = () =>{
+        toast.success(`${correctAnswer}`, {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
     }
 
 
@@ -50,14 +64,19 @@ const Quiz = ({data, index}) => {
 
     return (
         <div className='m-5 p-4 shadow p-3 mb-5 bg-body rounded'>
-            <h4>Quiz {index + 1}: {data.question}</h4>
+            <div className='mb-3 '>
+                <h4>Quiz {index + 1}: {data.question}</h4>
+                <button onClick={()=>eyeHandler()} type="button" className="btn btn-outline-success">
+                    <FontAwesomeIcon className='fs-4' icon={faEye}></FontAwesomeIcon>
+                </button>
+            </div>
             {
-                options.map((option, index)=> <Options
-                key={index}
-                index={index}
-                quizIndex={quizIndex}
-                option={option}
-                quizHandler={quizHandler}
+                options.map((option, index) => <Options
+                    key={index}
+                    index={index}
+                    quizIndex={quizIndex}
+                    option={option}
+                    quizHandler={quizHandler}
                 ></Options>)
             }
             <ToastContainer />
