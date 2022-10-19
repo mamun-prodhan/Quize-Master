b753208d29f0a3cee5c48b97cdd6510a461399b6
mamun-prodhan <mamunpr9@gmail.com>
2022-10-19 15:32:38 +0600

    correct & wrong answer completed

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -6,12 +6,25 @@ const Quiz = ({data, index}) => {
     // console.log(index);
     const options = data.options;
     const quizIndex = index;
+    const correctAnswer = data.correctAnswer;
+    console.log(correctAnswer);
     // console.log(options);
+    const [quizvalue, setQuizvalue] = useState();
 
+    // console.log(quizvalue);
 
     const quizHandler = (event) =>{
         console.log(event);
+        if(event === correctAnswer){
+            alert('correct');
+        }
+        else{
+            alert('wrong');
+        }
     }
+
+
+
 
     return (
         <div className='m-5 p-4 shadow p-3 mb-5 bg-body rounded'>
