d5f3a87889c8ea958c7a61352f04a396463d681d
mamun-prodhan <mamunpr9@gmail.com>
2022-10-19 14:44:23 +0600

    quiz options completed

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -3,12 +3,10 @@ import React from 'react';
 const Options = ({quizIndex,option,index,quizHandler}) => {
     // console.log(option);
     return (
-        <div className=''>
-            <section className='border p-4 text-start'>
-                <label>
-                     <input type="radio" id={index} name={quizIndex} value={option} onClick={()=>quizHandler(option)}/>{option}
+        <div className=' text-start'>
+                <label className='border border-primary w-100 p-3 m-2 bg-success bg-opacity-75'>
+                     <input className='me-2' type="radio" id={index} name={quizIndex} value={option} onClick={()=>quizHandler(option)}/>{option}
                 </label>
-            </section>
         </div>
     );
 };
